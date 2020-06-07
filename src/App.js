109824fbaddbb9c9fdcc6487d0f52d579db08e84
MuahmmadBilal <Bscs.f18.m.16.a@gmail.com>
2020-06-07 09:27:01 +0500

    putting break lines
    putting break lines

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './mystyles.css';
 function App(props) {
 return <div className="Hay0"><div className="Hay">Hello World!</div><br></br><div className="Hay">I am <strong>{props.name}</strong>.</div><div className="Hay">I am <strong>{props.age} years</strong> old.</div><h1 className="Hay1">My Friend includes:</h1><ol className="Hay1"><li>Ali</li><li>Hassan</li><li>Hussain</li><li>Umer</li><li>Asad</li></ol>
 <div className="Hay1">Working with &nbsp;&nbsp;&nbsp;&nbsp; space</div>
-<div className="Hay1">As we know that: 19 x 22 = {19*22}</div></div>
+<div className="Hay1">As we know that: 19 x 22 = {19*22}</div><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br></div>
 }
 
 export default App;
